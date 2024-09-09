5a6b45ddf619589a50110adfce4d77c40445dfee
Clement Lefebvre <clement.lefebvre@linuxmint.com>
2024-09-09 17:55:00 +0100

    console: Don't l10n CLI
    - Reduce the already huge effort needed for translators
    - Reduce support complexity by providing CLI tools which are always
    in English
    
    This is consistent with all the projects we maintain. All CLIs are
    in English for everybody. All UIs are fully translated in as many
    languages as possible.

--- aptkit/console.py
+++ aptkit/console.py
@@ -25,9 +25,6 @@ __all__ = ("ConsoleClient", "main")
 
 import array
 import fcntl
-from gettext import gettext as _
-from gettext import ngettext
-import locale
 from optparse import OptionParser
 import os
 import pty
@@ -212,7 +209,7 @@ class ConsoleClient:
 
         if enum == enums.EXIT_FAILED:
             msg = "%s: %s\n%s\n\n%s" % (
-                _("ERROR"),
+                "ERROR",
                 enums.get_error_string_from_enum(trans.error_code),
                 enums.get_error_description_from_enum(trans.error_code),
                 trans.error_details)
@@ -246,13 +243,13 @@ class ConsoleClient:
         """Callback for the ProgressDetails signal of the transaction."""
         if bytes_total and speed:
             self._progress_details = (
-                _("Downloaded %(cur)sB of %(total)sB at %(rate)sB/s") %
+                "Downloaded %(cur)sB of %(total)sB at %(rate)sB/s" %
                 {'cur': client.get_size_string(bytes_done),
                  'total': client.get_size_string(bytes_total),
                  'rate': client.get_size_string(speed)})
         elif bytes_total:
             self._progress_details = (
-                _("Downloaded %(cur)sB of %(total)sB") %
+                "Downloaded %(cur)sB of %(total)sB" %
                 {'cur': client.get_size_string(bytes_done),
                  'total': client.get_size_string(bytes_total)})
         else:
@@ -382,12 +379,10 @@ class ConsoleClient:
         try:
             raise error
         except errors.PolicyKitError:
-            msg = "%s %s\n\n%s" % (_("ERROR:"),
-                                   _("You are not allowed to perform "
-                                     "this action."),
+            msg = "%s %s\n\n%s" % ("ERROR:", "You are not allowed to perform this action.",
                                    error.get_dbus_message())
         except dbus.DBusException:
-            msg = "%s %s - %s" % (_("ERROR:"), error.get_dbus_name(),
+            msg = "%s %s - %s" % ("ERROR:", error.get_dbus_name(),
                                   error.get_dbus_message())
         except:
             msg = str(error)
@@ -403,18 +398,16 @@ class ConsoleClient:
                                       enums.ROLE_ADD_VENDOR_KEY_FROM_KEYSERVER,
                                       enums.ROLE_REMOVE_VENDOR_KEY,
                                       enums.ROLE_FIX_INCOMPLETE_INSTALL]:
-            # TRANSLATORS: status message
             self._progress_id = GLib.timeout_add(250,
                                                  self._update_custom_progress,
-                                                 _("Queuing"))
+                                                 "Queuing")
             self._transaction.run(
                 error_handler=self._on_exception,
                 reply_handler=lambda: self._stop_custom_progress())
         else:
-            # TRANSLATORS: status message
             self._progress_id = GLib.timeout_add(250,
                                                  self._update_custom_progress,
-                                                 _("Resolving dependencies"))
+                                                 "Resolving dependencies")
             self._transaction.simulate(reply_handler=self._show_changes,
                                        error_handler=self._on_exception)
 
@@ -455,86 +448,55 @@ class ConsoleClient:
         downgrades.extend(dep_downgrades)
         kepts = dep_kepts
         if installs:
-            # TRANSLATORS: %s is the number of packages
-            print((ngettext("The following NEW package will be installed "
-                            "(%(count)s):",
-                            "The following NEW packages will be installed "
-                            "(%(count)s):",
-                            len(installs)) % {"count": len(installs)}))
+            print("%d NEW packages will be installed:" % len(installs))
             show_packages(installs)
         if upgrades:
-            # TRANSLATORS: %s is the number of packages
-            print((ngettext("The following package will be upgraded "
-                            "(%(count)s):",
-                            "The following packages will be upgraded "
-                            "(%(count)s):",
-                            len(upgrades)) % {"count": len(upgrades)}))
+            print("%d packages will be upgraded:" len(upgrades))
             show_packages(upgrades)
         if removals:
-            # TRANSLATORS: %s is the number of packages
-            print((ngettext("The following package will be REMOVED "
-                            "(%(count)s):",
-                            "The following packages will be REMOVED "
-                            "(%(count)s):",
-                            len(removals)) % {"count": len(removals)}))
+            print("%d packages will be REMOVED:" len(removals))
             # FIXME: mark purges
             show_packages(removals)
         if downgrades:
-            # TRANSLATORS: %s is the number of packages
-            print((ngettext("The following package will be DOWNGRADED "
-                            "(%(count)s):",
-                            "The following packages will be DOWNGRADED "
-                            "(%(count)s):",
-                            len(downgrades)) % {"count": len(downgrades)}))
+            print("%d packages will be DOWNGRADED:" len(downgrades))
             show_packages(downgrades)
         if reinstalls:
-            # TRANSLATORS: %s is the number of packages
-            print((ngettext("The following package will be reinstalled "
-                            "(%(count)s):",
-                            "The following packages will be reinstalled "
-                            "(%(count)s):",
-                            len(reinstalls)) % {"count": len(reinstalls)}))
+            print("%d packages will be reinstalled:" len(reinstalls))
             show_packages(reinstalls)
         if kepts:
-            print((ngettext("The following package has been kept back "
-                            "(%(count)s):",
-                            "The following packages have been kept back "
-                            "(%(count)s):",
-                            len(kepts)) % {"count": len(kepts)}))
+            print("%d packages will be kept back:" len(kepts))
             show_packages(kepts)
 
         if self._transaction.download:
-            print(_("Need to get %sB of archives.") %
+            print("Need to get %sB of archives." %
                   client.get_size_string(self._transaction.download))
         if self._transaction.space > 0:
-            print(_("After this operation, %sB of additional disk space "
-                    "will be used.") %
+            print("After this operation, %sB of additional disk space "
+                    "will be used." %
                   client.get_size_string(self._transaction.space))
         elif self._transaction.space < 0:
-            print(_("After this operation, %sB of additional disk space "
-                    "will be freed.") %
+            print("After this operation, %sB of additional disk space "
+                    "will be freed." %
                   client.get_size_string(self._transaction.space))
         if (self._transaction.space or self._transaction.download or
                 installs or upgrades or downgrades or removals or kepts or
                 reinstalls):
             try:
                 if PY3K:
-                    cont = input(_("Do you want to continue [Y/n]?"))
+                    cont = input("Do you want to continue [Y/n]?")
                 else:
-                    cont = raw_input(_("Do you want to continue [Y/n]?"))
+                    cont = raw_input("Do you want to continue [Y/n]?")
             except EOFError:
                 cont = "n"
             # FIXME: Listen to changed dependencies!
-            if (not re.match(locale.nl_langinfo(locale.YESEXPR), cont) and
-                    cont != ""):
+            if (cont != "Y"):
                 msg = enums.get_exit_string_from_enum(enums.EXIT_CANCELLED)
                 self._update_custom_progress(msg, None, False)
                 self._loop.quit()
                 sys.exit(1)
-        # TRANSLATORS: status message
         self._progress_id = GLib.timeout_add(250,
                                              self._update_custom_progress,
-                                             _("Queuing"))
+                                             "Queuing")
         self._transaction.run(
             error_handler=self._on_exception,
             reply_handler=lambda: self._stop_custom_progress())
@@ -542,109 +504,106 @@ class ConsoleClient:
 
 def main():
     """Run a command line client for aptkit"""
-    epilog = _("To operate on more than one package put the package "
+    epilog = "To operate on more than one package put the package "
                "names in quotation marks:\naptkcon --install "
-               "\"foo bar\"")
+               "\"foo bar\""
     parser = OptionParser(version=aptkit.__version__, epilog=epilog)
     parser.add_option("-c", "--refresh", default="",
                       action="store_true", dest="refresh",
-                      help=_("Refresh the cache"))
+                      help="Refresh the cache")
     parser.add_option("", "--fix-depends", default="",
                       action="store_true", dest="fix_depends",
-                      help=_("Try to resolve broken dependencies. "
+                      help="Try to resolve broken dependencies. "
                              "Potentially dangerous operation since it could "
-                             "try to remove many packages."))
+                             "try to remove many packages.")
     parser.add_option("", "--fix-install", default="",
                       action="store_true", dest="fix_install",
-                      help=_("Try to finish a previous incompleted "
-                             "installation"))
+                      help="Try to finish a previous incompleted "
+                             "installation")
     parser.add_option("-i", "--install", default="",
                       action="store", type="string", dest="install",
-                      help=_("Install the given packages"))
+                      help="Install the given packages")
     parser.add_option("", "--reinstall", default="",
                       action="store", type="string", dest="reinstall",
-                      help=_("Reinstall the given packages"))
+                      help="Reinstall the given packages")
     parser.add_option("-r", "--remove", default="",
                       action="store", type="string", dest="remove",
-                      help=_("Remove the given packages"))
+                      help="Remove the given packages")
     parser.add_option("-p", "--purge", default="",
                       action="store", type="string", dest="purge",
-                      help=_("Remove the given packages including "
-                             "configuration files"))
+                      help="Remove the given packages including "
+                             "configuration files")
     parser.add_option("-u", "--upgrade", default="",
                       action="store", type="string", dest="upgrade",
-                      help=_("Install the given packages"))
+                      help="Install the given packages")
     parser.add_option("", "--downgrade", default="",
                       action="store", type="string", dest="downgrade",
-                      help=_("Downgrade the given packages"))
+                      help="Downgrade the given packages")
     parser.add_option("", "--upgrade-system",
                       action="store_true", dest="safe_upgrade",
-                      help=_("Deprecated: Please use "
-                             "--safe-upgrade"))
+                      help="Deprecated: Please use "
+                             "--safe-upgrade")
     parser.add_option("", "--safe-upgrade",
                       action="store_true", dest="safe_upgrade",
-                      help=_("Upgrade the system in a safe way"))
+                      help="Upgrade the system in a safe way")
     parser.add_option("", "--full-upgrade",
                       action="store_true", dest="full_upgrade",
-                      help=_("Upgrade the system, possibly installing and "
-                             "removing packages"))
+                      help="Upgrade the system, possibly installing and "
+                             "removing packages")
     parser.add_option("", "--add-vendor-key", default="",
                       action="store", type="string", dest="add_vendor_key",
-                      help=_("Add the vendor to the trusted ones"))
+                      help="Add the vendor to the trusted ones")
     parser.add_option("", "--add-vendor-key-from-keyserver", default="",
                       action="store", type="string",
-                      help=_("Add the vendor keyid (also needs "
-                             "--keyserver)"))
+                      help="Add the vendor keyid (also needs "
+                             "--keyserver)")
     parser.add_option("", "--keyserver", default="",
                       action="store", type="string",
-                      help=_("Use the given keyserver for looking up "
-                             "keys"))
+                      help="Use the given keyserver for looking up "
+                             "keys")
     parser.add_option("", "--add-repository", default="",
                       action="store", type="string", dest="add_repository",
-                      help=_("Add new repository from the given "
-                             "deb-line"))
+                      help="Add new repository from the given "
+                             "deb-line")
     parser.add_option("", "--sources-file", action="store", default="",
                       type="string", dest="sources_file",
-                      help=_("Specify an alternative sources.list.d file to "
-                             "which repositories should be added."))
+                      help="Specify an alternative sources.list.d file to "
+                             "which repositories should be added.")
     parser.add_option("", "--list-trusted-vendors", default="",
                       action="store_true", dest="list_trusted_vendor_keys",
-                      help=_("List trusted vendor keys"))
+                      help="List trusted vendor keys")
     parser.add_option("", "--remove-vendor-key", default="",
                       action="store", type="string", dest="remove_vendor_key",
-                      help=_("Remove the trusted key of the given "
-                             "fingerprint"))
+                      help="Remove the trusted key of the given "
+                             "fingerprint")
     parser.add_option("", "--clean",
                       action="store_true", dest="clean",
-                      help=_("Remove downloaded package files"))
+                      help="Remove downloaded package files")
     parser.add_option("", "--reconfigure", default="",
                       action="store", type="string", dest="reconfigure",
-                      help=_("Reconfigure installed packages. Optionally the "
+                      help="Reconfigure installed packages. Optionally the "
                              "minimum priority of questions can be "
-                             "specified"))
+                             "specified")
     parser.add_option("", "--priority", default="default",
                       action="store", type="string", dest="priority",
-                      help=_("The minimum debconf priority of question to "
-                             "be displayed"))
+                      help="The minimum debconf priority of question to "
+                             "be displayed")
     parser.add_option("", "--hide-terminal",
                       action="store_true", dest="hide_terminal",
-                      help=_("Do not attach to the apt terminal"))
+                      help="Do not attach to the apt terminal")
     parser.add_option("", "--allow-unauthenticated",
                       action="store_true", dest="allow_unauthenticated",
                       default=False,
-                      help=_("Allow packages from unauthenticated "
-                             "sources"))
+                      help="Allow packages from unauthenticated sources")
     parser.add_option("-d", "--show-details",
                       action="store_true", dest="details",
-                      help=_("Show additional information about the packages. "
-                             "Currently only the version number"))
+                      help="Show additional information about the packages. "
+                             "Currently only the version number")
     (options, args) = parser.parse_args()
     con = ConsoleClient(show_terminal=not options.hide_terminal,
                         allow_unauthenticated=options.allow_unauthenticated,
                         details=options.details)
-    # TRANSLATORS: status message
-    con._progress_id = GLib.timeout_add(250, con._update_custom_progress,
-                                        _("Waiting for authentication"))
+    con._progress_id = GLib.timeout_add(250, con._update_custom_progress, "Waiting for authentication")
     if options.safe_upgrade:
         con.upgrade_system(True)
     elif options.full_upgrade:
